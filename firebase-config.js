b2f62011c9aab8acbd2f2a323570d0d5e1f05e82
InsharahMuallim <insharahmuallim1@gmail.com>
2025-10-31 17:27:27 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,10 +5,9 @@ const firebaseConfig = {
   databaseURL: "https://smart-fleet-management-s-832af-default-rtdb.firebaseio.com",
   projectId: "smart-fleet-management-s-832af",
   storageBucket: "smart-fleet-management-s-832af.appspot.com",
-  messagingSenderId: "245808193086",
-  appId: "1:245808193086:web:ea1cf8098219bb9ac4bb6f"
+  messagingSenderId: "1081234567890",
+  appId: "1:1081234567890:web:abcdef123456"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
+console.log("✅ Firebase connected successfully!");
